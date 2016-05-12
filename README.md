e0becc3c1bb9f438e4c9cc42ce7fa286dfdbc584
Lucas Clemente <lucas@clemente.io>
2016-05-12 18:35:03 +0200

    improve webpage

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Important commands:
 
 ```sh
-go build -o main && sudo docker build -t lclemente/quic-demo . && sudo docker run --rm -it --d=NET_ADMIN -p 8000-8009:8000-8009 -p 8000-8009:8000-8009/udp -v /etc/letsencrypt/live/quic.clemente.io:/certs lclemente/quic-demo
+go build -o main && sudo docker build -t lclemente/quic-demo . && sudo docker run --rm -it --cap-add=NET_ADMIN -p 8000-8009:8000-8009 -p 8000-8009:8000-8009/udp -v /etc/letsencrypt/live/quic.clemente.io:/certs lclemente/quic-demo
 
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome  --user-data-dir=/tmp/chrome --origin-to-force-quic-on=quic.clemente.io:8005,quic.clemente.io:8006,quic.clemente.io:8007,quic.clemente.io:8008,quic.clemente.io:8009 --enable-quic https://quic.clemente.io:8000
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome  --user-data-dir=/tmp/chrome --origin-to-force-quic-on=quic.clemente.io:8000,quic.clemente.io:8005,quic.clemente.io:8006,quic.clemente.io:8007,quic.clemente.io:8008,quic.clemente.io:8009 --enable-quic https://quic.clemente.io:8000
 ```
--- main.go
+++ main.go
@@ -15,38 +15,82 @@ const index = `
 <head>
   <title>QUIC Demo</title>
   <style>
-    iframe {
+		body {
+			font-family: sans-serif;
+			margin: 1em;
+		}
+
+		code {
+			display: block;
+			margin: 2em;
+		}
+
+    .demo {
       width: 100px;
       height: 100px;
+			text-align: center;
     }
+
+		footer {
+			margin-top: 2em;
+		}
   </style>
 </head>
 <body>
+	<h1>QUIC Test Page</h1>
+
+	<p>
+		This webpage uses an experimental <a href="https://github.com/lucas-clemente/quic-go">Go implemention</a> of the novel <a href="https://en.wikipedia.org/wiki/QUIC">QUIC protocol</a> proposed by Google.
+	</p>
+	<p>
+		To use it, you need to use a current Chrome version, and launch it using a special command from the console. For OS X, try:
+		<code>
+			/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --user-data-dir=/tmp/chrome --origin-to-force-quic-on=quic.clemente.io:8000,quic.clemente.io:8005,quic.clemente.io:8006,quic.clemente.io:8007,quic.clemente.io:8008,quic.clemente.io:8009 --enable-quic https://quic.clemente.io:8000
+		</code>
+
+		For linux, try:
+		<code>
+			chrome --user-data-dir=/tmp/chrome --origin-to-force-quic-on=quic.clemente.io:8000,quic.clemente.io:8005,quic.clemente.io:8006,quic.clemente.io:8007,quic.clemente.io:8008,quic.clemente.io:8009 --enable-quic https://quic.clemente.io:8000
+		</code>
+	</p>
+
+	<h2>Simulated Round-Trip-Time (RTT)</h2>
+
+	<p>
+		QUIC features a custom crypto stack that is able to achieve connection in 0-1 RTTs. TCP+TLS usually needs 3-4 RTTs. If something doesn't work, just try reloading :)
+	</p>
+
   <table>
     <tr>
-      <th></th>
+      <th>Simulated RTT</th>
       <th>0 ms</th>
       <th>100 ms</th>
       <th>500 ms</th>
       <th>1 s</th>
     </tr>
     <tr>
-      <td>TCP+TLS</td>
-      <td><iframe src="https://quic.clemente.io:8000/test"></iframe></td>
-      <td><iframe src="https://quic.clemente.io:8001/test"></iframe></td>
-      <td><iframe src="https://quic.clemente.io:8003/test"></iframe></td>
-      <td><iframe src="https://quic.clemente.io:8004/test"></iframe></td>
+      <td>TCP+TLS+HTTP2</td>
+      <td><iframe class="demo" src="https://quic.clemente.io:8000/test"></iframe></td>
+      <td><iframe class="demo" src="https://quic.clemente.io:8001/test"></iframe></td>
+      <td><iframe class="demo" src="https://quic.clemente.io:8003/test"></iframe></td>
+      <td><iframe class="demo" src="https://quic.clemente.io:8004/test"></iframe></td>
     </tr>
     <tr>
       <td>QUIC</td>
-      <td><iframe src="https://quic.clemente.io:8005/test"></iframe></td>
-      <td><iframe src="https://quic.clemente.io:8006/test"></iframe></td>
-      <td><iframe src="https://quic.clemente.io:8008/test"></iframe></td>
-      <td><iframe src="https://quic.clemente.io:8009/test"></iframe></td>
+      <td><iframe class="demo" src="https://quic.clemente.io:8005/test"></iframe></td>
+      <td><iframe class="demo" src="https://quic.clemente.io:8006/test"></iframe></td>
+      <td><iframe class="demo" src="https://quic.clemente.io:8008/test"></iframe></td>
+      <td><iframe class="demo" src="https://quic.clemente.io:8009/test"></iframe></td>
     </tr>
     <tr>
     </tr>
   </table>
+
+
+	<footer>
+		<hr/>
+		Built by Lucas Clemente and Marten Seemann, see <a href="https://github.com/lucas-clemente/quic-go">GitHub</a>.
+	</footer>
 </body>
 </html>
 `
@@ -56,7 +100,18 @@ func indexHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func testHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "<body bgcolor=green>Loaded</body>")
+	io.WriteString(w, `<body style="font-family: sans-serif; background-color: #5cb85c; color: white; text-align: center; padding-top: 30px;">Loaded</body>`)
+}
+
+const timeout = 4 * time.Second
+
+func runH2serverWithTimeout(port string, handler http.Handler) {
+	h2server := &http.Server{
+		Addr:    ":" + port,
+		Handler: handler,
+	}
+	h2server.ReadTimeout = timeout
+	h2server.ListenAndServeTLS("/certs/cert.pem", "/certs/privkey.pem")
 }
 
 func startServer(port string) {
@@ -64,50 +119,52 @@ func startServer(port string) {
 		Addr:    ":" + port,
 		Handler: nil,
 	}
-	server.ReadTimeout = 3 * time.Second
+	server.ReadTimeout = timeout
 	server.ListenAndServeTLS("/certs/cert.pem", "/certs/privkey.pem")
 }
 
 var quicServer *h2quic.Server
+var quicServeMux *http.ServeMux
 
 func startQuicServer(port string) {
-	go func() {
-		err := quicServer.ListenAndServe(":"+port, nil)
-		if err != nil {
-			panic(err)
-		}
-	}()
-
-	h2server := &http.Server{
-		Addr: ":" + port,
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	go runH2serverWithTimeout(
+		port,
+		http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 			w.Header().Set("Alternate-Protocol", port+":quic")
-			io.WriteString(w, "Not QUIC, refresh")
+			io.WriteString(w, "Not using QUIC, see instructions above.")
 		}),
+	)
+	err := quicServer.ListenAndServe(":"+port, nil)
+	if err != nil {
+		panic(err)
 	}
-	h2server.ReadTimeout = 3 * time.Second
-	h2server.ListenAndServeTLS("/certs/cert.pem", "/certs/privkey.pem")
 }
 
 func main() {
-	tlsConfig := testdata.GetTLSConfig()
 	var err error
+	tlsConfig := testdata.GetTLSConfig()
+
 	quicServer, err = h2quic.NewServer(tlsConfig)
 	if err != nil {
 		panic(err)
 	}
 
+	quicServeMux = http.NewServeMux()
+	quicServeMux.HandleFunc("/test", testHandler)
+	quicServeMux.HandleFunc("/", indexHandler)
+
 	utils.SetLogLevel(utils.LogLevelInfo)
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/test", testHandler)
-	go startServer("8001")
-	go startServer("8002")
-	go startServer("8003")
-	go startServer("8004")
+	go runH2serverWithTimeout("8001", nil)
+	go runH2serverWithTimeout("8002", nil)
+	go runH2serverWithTimeout("8003", nil)
+	go runH2serverWithTimeout("8004", nil)
 	go startQuicServer("8005")
 	go startQuicServer("8006")
 	go startQuicServer("8007")
 	go startQuicServer("8008")
 	go startQuicServer("8009")
-	startServer("8000")
+	go startQuicServer("8000")
+	runH2serverWithTimeout("8000", nil)
 }
